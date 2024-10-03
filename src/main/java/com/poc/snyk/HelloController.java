1ae8155ed59711b0621c7de415f999e58861d050
Gulshankmr <gulshankumar454@gmail.com>
2024-10-03 17:45:11 +0530

    initialCommit

---
+++ src/main/java/com/poc/snyk/HelloController.java
@@ -0,0 +1,35 @@
+package com.poc.snyk;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, I am a Spring Boot app with Snyk integration!";
+    }
+
+    @GetMapping("/name")
+    public String TEST_NAMING_CONVENION() {
+        String x = "HELLO_WORLD";
+        return x;
+    }
+
+    @PostMapping("/")
+    public int TEST_Exception() {
+        int x = 5 / 0;
+        return x;
+    }
+
+    @PostMapping("/try")
+    public int TEST_tryCatch() {
+        try {
+            int x = 5 / 0;
+            return x;
+        } catch (Exception ex) {
+        }
+        return 0;
+    }
+}
