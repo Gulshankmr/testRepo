c9228f6958c7b24784121e65990f47105828f73d
Gulshankmr <gulshankumar454@gmail.com>
2024-10-04 11:44:04 +0530

    Random commit

--- src/main/java/com/poc/snyk/HelloController.java
+++ src/main/java/com/poc/snyk/HelloController.java
@@ -4,11 +4,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.DriverManager;
+
 @RestController
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "Hello, I am a Spring Boot app with Snyk integration!";
+        int sportType = 2;
+        boolean isSubstitute = true;
+        boolean isSelected = false;
+        int score = 0;
+        switch (sportType) {
+            case 2: {
+                if (isSubstitute) {
+                    score += 2;
+                } else if (isSelected) {
+                    score += 4;
+                }
+            }
+            case 3: {
+                if (isSubstitute) {
+                    score += 4;
+                } else if (isSelected) {
+                    score += 8;
+                }
+            }
+            default: {
+            }
+        }
+        return "Hello, I am a Spring Boot app with Snyk integration! " + score;
     }
 
     @GetMapping("/name")
@@ -31,6 +55,19 @@ public class HelloController {
         try {
             int x = 5 / 0;
             return x;
+        } catch (Exception ex) {
+        }
+        return 0;
+    }
+
+    @PostMapping("/creds")
+    public int hardcodeCreds() {
+        try {
+            String serverip = "localhost";
+            String serverport = "1433";
+            String url = "jdbc:sqlserver://" + serverip + "\\SQLEXPRESS:" + serverport + ";databaseName=" + "test" + "";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            DriverManager.getConnection(url, "admin", "qwerty");
         } catch (Exception ex) {
         }
         return 0;
